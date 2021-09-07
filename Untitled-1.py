60bb3356d6c56ef568cc85a0afff4c6429f7d679
Agustin-PerezR <perezragustin@gmail.com>
2021-09-07 19:15:13 -0300

    l

---
+++ Untitled-1.py
@@ -0,0 +1,2 @@
+mensaje = "!Hola Mundo¡"
+print(mensaje)
---
+++ pagina.js
@@ -0,0 +1,11 @@
+let e;
+/*window.addEventListener("scroll", ()=> {
+    const total= window.scrollY;
+    console.log(total);
+    if(total => 100 ){
+        document.getElementById("trabajos").style.backgroundImage="linear-gradient(90deg, rgb(0, 36, 212), rgb(218, 135, 125))"
+    }
+    else if(total => 600){
+        document.getElementById("trabajo").style.backgroundImage="linear-gradient(45deg, rgb(136, 136, 245), rgb(248, 135, 135))"
+    }
+});*/
